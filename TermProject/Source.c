f2e798807cbb101763615ce52471999f6e13abb4
Unhelpfu1 <krogstad.josh@gmail.com>
2025-09-18 01:21:18 -0400

    added parsing for each command

--- TermProject/Source.c
+++ TermProject/Source.c
@@ -2,59 +2,184 @@
 #include <string.h>
 #include <stdlib.h>
 
+//Parse each individual command for errors
+void parseCommandUpload(char* secondToken, char* thirdToken) {
+	if (secondToken == NULL || strcmp(secondToken, "") == 0 || thirdToken == NULL || strcmp(thirdToken, "\n") == 0) { // Not enough arguments
+		printf("  >> Syntax error: upload <local filename> <remote filename>\n");
+	}
+	else {
+		printf("  >> Upload command received\n");
+	}
+}
+
+void parseCommandDownload(char* secondToken, char* thirdToken) {
+	if (secondToken == NULL || strcmp(secondToken, "") == 0 || thirdToken == NULL || strcmp(thirdToken, "\n") == 0) { // Not enough arguments
+		printf("  >> Syntax error: download <local filename> <remote filename>\n");
+	}
+	else {
+		printf("  >> Download command received\n");
+	}
+}
+
+void parseCommandDelete(char* secondToken, char* thirdToken) {
+	if (secondToken == NULL || strcmp(secondToken, "") == 0 || thirdToken == NULL || strcmp(thirdToken, "\n") == 0) { // Not enough arguments
+		printf("  >> Syntax error: delete [local/remote] <filename>\n");
+	}
+	else if (strcmp(secondToken, "local") == 0) {
+		printf("  >> Delete Local command received\n");
+	}
+	else if (strcmp(secondToken, "remote") == 0) {
+		printf("  >> Delete Remote command received\n");
+	}
+	else {
+
+		printf("  >> \'%s\' is not recognized. Valid options are \'local\' and \'remote\'.\n", secondToken);
+	}
+}
+
+void parseCommandChange(char* secondToken, char* thirdToken) {
+	if (secondToken == NULL || strcmp(secondToken, "") == 0 || thirdToken == NULL || strcmp(thirdToken, "\n") == 0) { // Not enough arguments
+		printf("  >> Syntax error: change [local/remote] <filepath>\n");
+	}
+	else if (strcmp(secondToken, "local") == 0) {
+		printf("  >> Change Local command received\n");
+	}
+	else if (strcmp(secondToken, "remote") == 0) {
+		printf("  >> Change Remote command received\n");
+	}
+	else {
+		printf("  >> \'%s\' is not recognized. Valid options are \'local\' and \'remote\'.\n", secondToken);
+	}
+}
+
+void parseCommandShow(char* secondToken, char* thirdToken) {
+	if (secondToken == NULL || strcmp(secondToken, "") == 0 || thirdToken == NULL || strcmp(thirdToken, "\n") == 0) { // Not enough arguments
+		printf("  >> Syntax error: show [local/remote] [path/files/folders]\n");
+	}
+	else if (strcmp(secondToken, "local") == 0) {
+		if (strcmp(thirdToken, "path\n") == 0) {
+			printf("  >> Show local path command received\n");
+		}
+		else if (strcmp(thirdToken, "files\n") == 0) {
+			printf("  >> Show local files command received\n");
+		}
+		else if (strcmp(thirdToken, "folders\n") == 0) {
+			printf("  >> Show local folders command received\n");
+		}
+		else {
+			printf("  >> \'%s\' is not recognized. Valid options are \'path\', \'files\', or \'folders\'.\n", thirdToken);
+		}
+	}
+	else if (strcmp(secondToken, "remote") == 0) {
+		if (strcmp(thirdToken, "path\n") == 0) {
+			printf("  >> Show remote path command received\n");
+		}
+		else if (strcmp(thirdToken, "files\n") == 0) {
+			printf("  >> Show remote files command received\n");
+		}
+		else if (strcmp(thirdToken, "folders\n") == 0) {
+			printf("  >> Show remote folders command received\n");
+		}
+		else {
+			printf("  >> \'%s\' is not recognized. Valid options are \'path\', \'remote\', or \'folders\'.\n", thirdToken);
+		}
+	}
+	else {
+		printf("  >> \'%s\' is not recognized. Valid options are \'local\' and \'remote\'.\n", secondToken);
+	}
+}
+
+void parseCommandHistory(char* secondToken, char* thirdToken) {
+	if (secondToken != NULL || thirdToken != NULL) { // Too many arguments
+		printf("  >> Syntax error: \'history\' takes no parameters.\n");
+	}
+	else {
+		printf("  >> history command received\n");
+	}
+}
+
+void parseCommandValidate(char* secondToken, char* thirdToken) {
+	if (secondToken != NULL || thirdToken != NULL) { // Too many arguments
+		printf("  >> Syntax error: \'validate\' takes no parameters.\n");
+	}
+	else {
+		printf("  >> validate command received\n");
+	}
+}
+
+void parseCommandQuit(char* secondToken, char* thirdToken) {
+	if (secondToken != NULL || thirdToken != NULL) { // Too many arguments
+		printf("  >> Syntax error: \'quit\' takes no parameters.\n");
+	}
+	else {
+		printf("  >> Quitting...\n");
+	}
+}
+
 // parsing
 void parseInput(char* userInput) {
-	char* firstToken = strtok(userInput, " ");
-	char* secondToken = strtok(NULL, " ");
-	char* thirdToken = strtok(NULL, " ");
+	const char delimiters[] = " \t\n\r";
+	char* firstToken = strtok(userInput, delimiters);
+	char* secondToken = strtok(NULL, delimiters);
+	char* thirdToken = strtok(NULL, delimiters);
 	//printf(">> Command received: %s %s %s", firstToken, secondToken, thirdToken); // DEBUG
+	if (strtok(NULL, " ") != NULL) {
 
-	// Check firstToken against valid input
-	if ((strcmp(firstToken, "upload") == 0) || (strcmp(firstToken, "upload\n") == 0)) {
-
-		printf("Received Upload\n");
-
-	} else if ((strcmp(firstToken, "download")) == 0 || (strcmp(firstToken, "download\n") == 0)) {
-
-		printf("download\n");
-
+		printf("  >> Syntax error: Too many arguments\n");
+	
 	}
-	else if ((strcmp(firstToken, "delete") == 0) || (strcmp(firstToken, "delete\n") == 0)) {
+	else {
+		// Check firstToken against valid input
+		if ((strcmp(firstToken, "upload") == 0) || (strcmp(firstToken, "upload\n") == 0)) {
 
-		printf("delete\n");
+			parseCommandUpload(secondToken, thirdToken);
 
-	}
-	else if ((strcmp(firstToken, "change") == 0) || (strcmp(firstToken, "change\n") == 0)) {
+		} 
+		else if ((strcmp(firstToken, "download")) == 0 || (strcmp(firstToken, "download\n") == 0)) {
 
-		printf("change\n");
+			parseCommandDownload(secondToken, thirdToken);
 
-	}
-	else if ((strcmp(firstToken, "show") == 0) || (strcmp(firstToken, "show\n") == 0)) {
+		}
+		else if ((strcmp(firstToken, "delete") == 0) || (strcmp(firstToken, "delete\n") == 0)) {
 
-		printf("show\n");
+			parseCommandDelete(secondToken, thirdToken);
 
-	}
-	else if ((strcmp(firstToken, "history") == 0) || (strcmp(firstToken, "history\n") == 0)) {
+		}
+		else if ((strcmp(firstToken, "change") == 0) || (strcmp(firstToken, "change\n") == 0)) {
 
-		printf("history\n");
+			parseCommandChange(secondToken, thirdToken);
 
-	}
-	else if ((strcmp(firstToken, "validate") == 0) || (strcmp(firstToken, "validate\n") == 0)) {
+		}
+		else if ((strcmp(firstToken, "show") == 0) || (strcmp(firstToken, "show\n") == 0)) {
 
-		printf("validate\n");
+			parseCommandShow(secondToken, thirdToken);
 
-	}
-	else if ((strcmp(firstToken, "quit") == 0) || (strcmp(firstToken, "quit\n") == 0)) {
+		}
+		else if ((strcmp(firstToken, "history") == 0) || (strcmp(firstToken, "history\n") == 0)) {
 
-		printf("  >> Quitting\n");
+			parseCommandHistory(secondToken, thirdToken);
 
-	}
-	else if ((strcmp(firstToken, "\\0") == 0) || (strcmp(firstToken, "\n") == 0)) {
+		}
+		else if ((strcmp(firstToken, "validate") == 0) || (strcmp(firstToken, "validate\n") == 0)) {
 
-		printf("No command received\n");
+			parseCommandValidate(secondToken, thirdToken);
 
-	}else {
-		printf("Command unrecognized\n");
+		}
+		else if ((strcmp(firstToken, "quit") == 0) || (strcmp(firstToken, "quit\n") == 0)) {
+
+			parseCommandQuit(secondToken, thirdToken);
+
+		}
+		else if ((strcmp(firstToken, "\\0") == 0) || (strcmp(firstToken, "\n") == 0)) {
+
+			printf("No command received\n");
+
+		}
+		else {
+
+			printf("Command unrecognized\n");
+
+		}
 	}
 
 	//Newline for aesthetics
@@ -63,19 +188,24 @@ void parseInput(char* userInput) {
 
 void main(void) {
 
-	char* userInput[100];
+	char userInput[100];
 
 	// Until user quits
-	while (strcmp(userInput, "quit\n") != 0) {
+	while (1) {
 
 		// Ask for user input
 		printf("FML> ");
 		fgets(userInput, 100, stdin);
-		//printf(userInput); // DEBUG
+		//printf("%s\n", userInput); // DEBUG
 
 		// send input to parse
 		parseInput(userInput);
-
 		// print result based on parsing
+		
+
+		// Escape when user enters 'quit'
+		if (strcmp(userInput, "quit\n") == 0 || strcmp(userInput, "quit") == 0) {
+			break;
+		}
 	}
 }
